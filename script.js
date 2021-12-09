4043f14d538a88020d81d09c9f6b33c6f7b45fab
AltaraceO <orialtarace@gmail.com>
2021-12-09 18:48:02 +0200

    creating buttons

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const mainSec = document.querySelector(".main");
 
-continentArr = [
-  { continant: { name: "Asia", countries: [] } },
-  { continant: { name: "Africa", countries: [] } },
-  { continant: { name: "Europe", countries: [] } },
-  { continant: { name: "Americas", countries: [] } },
-  { continant: { name: "Oceania", countries: [] } },
+const continentArr = [
+  { continent: { name: "Asia", countries: [] } },
+  { continent: { name: "Africa", countries: [] } },
+  { continent: { name: "Europe", countries: [] } },
+  { continent: { name: "Americas", countries: [] } },
+  { continent: { name: "Oceania", countries: [] } },
 ];
+const names = ["asia", "africa", "europe", "americas", "oceania"];
 
-function continantNames() {
-  const names = ["asia", "africa", "europe", "americas", "oceania"];
-
+function continentNames() {
   names.forEach((name) => {
+    makeButtons(name);
     getCountryData(name);
   });
 }
 
+function makeButtons(name) {
+  const contiContainer = document.createElement("div");
+  //   contiContainer.setAttribute(`id`, `${name}`);
+  //   contiContainer.id = `${name}`;
+  //   contiContainer.classList.add(`cont`);
+  mainSec.appendChild(contiContainer);
+  const btn = document.createElement("button");
+  //   btn.innerText = `${name}`;
+  contiContainer.appendChild(btn);
+}
+
 async function getCountryData(name) {
-  const response = await axios.get(
-    `https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${name}`
-  );
-  console.log(response.data);
-  getCountNameNRegion(response.data);
+  try {
+    const response = await axios.get(
+      `https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${name}`
+    );
+    getCountNameNRegion(response.data);
+  } catch (e) {
+    console.log(`error!!! ${e}`);
+  }
 }
 
 function getCountNameNRegion(data) {
   data.forEach((country) => {
-    // console.log(country.name.common, country.region);
+    let countryCode = country.cca2;
     let countName = country.name.common;
     let region = country.region;
     newObj = {
       name: countName,
       region: region,
+      country_code: countryCode,
     };
     pushCountries(newObj);
   });
@@ -39,14 +54,31 @@ function getCountNameNRegion(data) {
 
 function pushCountries(obj) {
   continentArr.forEach((el) => {
-    // console.log(el.continant.name);
-    if (obj.region === el.continant.name) {
-      console.log("yes");
-      el.continant.countries.push(obj);
-    } else {
-      //   console.log(el.continant.name, obj.name);
+    if (obj.region === el.continent.name) {
+      el.continent.countries.push(obj);
     }
   });
 }
-continantNames();
+createDropdown();
+
+continentNames();
 console.log(continentArr);
+
+function makeButtons(name) {
+  const contiContainer = document.createElement("div");
+  contiContainer.classList.add(`${name}`);
+  contiContainer.classList.add(`cont`);
+  mainSec.appendChild(contiContainer);
+  const btn = document.createElement("button");
+  btn.innerText = `${name}`;
+  contiContainer.appendChild(btn);
+}
+
+function createDropdown() {
+  window.addEventListener("load", (e) => {
+    const btns = document.querySelectorAll(".cont");
+    btns.forEach((e) => {
+      console.log(e);
+    });
+  });
+}
